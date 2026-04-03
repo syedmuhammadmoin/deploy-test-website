8ece63afa683d54d37d857d856c9015e6843568d
Moin <syedmuhammadmoin@gmail.com>
2026-04-03 17:19:34 +0500

    fix enable ssl for rds connection

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
+  ssl: { rejectUnauthorized: false },
 });
 
 // Health check
